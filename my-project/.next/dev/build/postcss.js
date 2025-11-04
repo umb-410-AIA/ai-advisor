b0063ac8488bc6e7ebcba9aa8a02131f825fab86
Vraj Soni <vrajsoni@Vrajs-Mac-mini.local>
2025-11-04 04:27:14 -0500

    modified Graphical Representations in the chatbox also added visual enhancements

---
+++ my-project/.next/dev/build/postcss.js
@@ -0,0 +1,6 @@
+var R=require("./chunks/[turbopack]_runtime.js")("postcss.js")
+R.c("chunks/[turbopack-node]_transforms_postcss_ts_882c8787._.js")
+R.c("chunks/[root-of-the-server]__3f973562._.js")
+R.m("[turbopack-node]/globals.ts [postcss] (ecmascript)")
+R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/ai-advisor/my-project/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
+module.exports=R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/ai-advisor/my-project/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
---
+++ my-project/.next/dev/static/chunks/ai-advisor_my-project_pages__error_2da965e7._.js
@@ -0,0 +1,15 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/8c0d4_next_dist_compiled_71e08783._.js",
+  "static/chunks/8c0d4_next_dist_shared_lib_95d6683c._.js",
+  "static/chunks/8c0d4_next_dist_client_2b0fc69a._.js",
+  "static/chunks/8c0d4_next_dist_3d511b68._.js",
+  "static/chunks/8c0d4_next_error_8017013c.js",
+  "static/chunks/[next]_entry_page-loader_ts_34e61333._.js",
+  "static/chunks/8c0d4_react-dom_40127a87._.js",
+  "static/chunks/8c0d4_074fe9ba._.js",
+  "static/chunks/[root-of-the-server]__fd9da48f._.js"
+],
+    source: "entry"
+});
