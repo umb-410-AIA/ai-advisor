5eefbb344824d329ed5eda4856ae27986e0d3f80
kepatel <keertan.patel001@umb.edu>
2025-12-04 16:06:00 -0500

    UMB course data fully sorted

--- scripts/umass_cs_catalog_filter.py
+++ scripts/umass_cs_catalog_filter.py
@@ -8,10 +8,10 @@ Simple script that filters the data in the umass boston course catalog to have o
 
 import json
 
-DEFAULT_INPUT_FILE = "../data/UMASS_BOSTON_coursecatalogstructured.json"
-DEFAULT_OUTPUT_FILE = "../data/UMASS_BOSTON_CS_courses.json"
+DEFAULT_INPUT_FILE = "../data/UMB/UMASS_BOSTON_coursecatalogstructured.json"
+DEFAULT_OUTPUT_PATH = "../data/UMB/"
 
-def filter_cs_courses(input_file=DEFAULT_INPUT_FILE, output_file=DEFAULT_OUTPUT_FILE):
+def filter_cs_courses(input_file=DEFAULT_INPUT_FILE, output_file=DEFAULT_OUTPUT_PATH + "CS_courses.json"):
     with open(input_file, 'r', encoding= "utf-8") as infile:
         courses = json.load(infile)
 
@@ -29,6 +29,47 @@ def filter_cs_courses(input_file=DEFAULT_INPUT_FILE, output_file=DEFAULT_OUTPUT_
         json.dump(cs_courses, outfile, indent=2, ensure_ascii= False)
 
 
+def filter_all_courses(input_file=DEFAULT_INPUT_FILE, output_path=DEFAULT_OUTPUT_PATH, dept_codes=[]):
+    with open(input_file, 'r', encoding="utf-8") as infile:
+        courses = json.load(infile)
+    for dept in dept_codes:
+        dept_courses = []
+        for course in courses:
+            if not isinstance(course, dict):
+                continue
+            courseid = str(course.get("courseid", ""))
+            if courseid.startswith(dept) and not courseid.startswith("CSP"):
+                dept_courses.append(course)
+        if dept_courses:
+            with open(output_path + f"{dept}_courses.json", "w", encoding="utf-8") as outfile:
+                json.dump(dept_courses, outfile, indent=2, ensure_ascii=False)
+            
+    
+    
+
+
+def get_dept_codes(input_file=DEFAULT_INPUT_FILE, output_file=DEFAULT_OUTPUT_PATH + "UMASS_BOSTON_CS_courses.json"):
+    with open(input_file, 'r', encoding="utf-8") as infile:
+        courses = json.load(infile)
+    dept_codes = []
+
+    for course in courses:
+        if not isinstance(course, dict):
+            continue
+        courseid = str(course.get("courseid", ""))
+        dept_id = extract_dept_from_string(courseid)
+        if dept_id not in dept_codes:
+            dept_codes.append(dept_id)
+    return dept_codes
+        
+
+def extract_dept_from_string(x):
+    code = ""
+    for i in x:
+        if i.isalpha():
+            code += i
+    return code
 
 if __name__ == "__main__":
-    filter_cs_courses()
+    dept_codes = get_dept_codes()
+    filter_all_courses(dept_codes=dept_codes)
